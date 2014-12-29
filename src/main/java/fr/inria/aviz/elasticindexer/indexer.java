06e08e9947ac27a47c4667ff818fcc1aeaee649e
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2014-12-29 14:19:06 +0100

    continue

--- src/main/java/fr/inria/aviz/elasticindexer/indexer.java
+++ src/main/java/fr/inria/aviz/elasticindexer/indexer.java
@@ -1,11 +1,60 @@
 package fr.inria.aviz.elasticindexer;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.tika.Tika;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
 
 /**
- * Main class, indexes a document
+ * Singleton class, indexes documents.
  */
 class Indexer {
-    private static final Tika tika = new Tika();
+    private static Indexer instance_;
+    private final Tika tika = new Tika();
+    private final Properties props = new Properties();
+    private Client es;
+    private Node node;
+
+    private Indexer() {
+        try {
+            FileInputStream in = new FileInputStream("elasticindexer.properties");
+            props.load(in);
+            in.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+//        TransportClient tes = new TransportClient();
+//        for (int i = 1; i < 100; i++) {
+//            String prop = "host"+i, 
+//                    host = props.getProperty(prop);
+//            if (host == null) break;
+//            tes.addTransportAddress(new InetSocketTransportAddress(host, 9300));
+//        }
+//        es = tes;
+        node = NodeBuilder.nodeBuilder().node();
+        es = node.client();
+    }
+    
+    private void close() {
+        node.close();
+    }
+    
+    public static Indexer instance() {
+        if (instance_ == null) {
+            instance_ = new Indexer();
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                public void run() {
+                    instance_.close();
+                    instance_ = null;
+                }
+            }));
+        }
+        return instance_;
+    }
 };
 
