e043d52dfd92a6d2a15dd6f2dc3d0a261f5f0d56
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2014-12-30 19:11:56 +0100

    add tika test to suite

--- src/test/java/fr/inria/aviz/elasticindexer/AllTests.java
+++ src/test/java/fr/inria/aviz/elasticindexer/AllTests.java
@@ -12,7 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * Run all tests in the right order
  */
 @RunWith(Suite.class)
-@SuiteClasses({ TestDocumentJSON.class, TestESConnection.class, TestIndexer.class })
+@SuiteClasses({ TestDocumentJSON.class, TestESConnection.class, TestIndexer.class, TestTika.class })
 public class AllTests {
 
 } 
