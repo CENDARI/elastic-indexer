46b49362c8b7159a1b0c8bdf97e33b05253ade41
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-01-15 18:31:01 +0100

    add cendari api test

---
+++ src/test/java/fr/inria/aviz/elasticindexer/TestCendariAPI.java
@@ -0,0 +1,73 @@
+package fr.inria.aviz.elasticindexer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import fr.inria.aviz.elasticindexer.ckan.CendariIndexer;
+import fr.inria.aviz.elasticindexer.ckan.Resource;
+import fr.inria.aviz.elasticindexer.ckan.ResourceList;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Class TestCendariAPI
+ * 
+ * @author Jean-Daniel Fekete
+ * @version $Revision$
+ */
+public class TestCendariAPI extends TestCase {
+    @Test
+    public void test() {
+        Indexer indexer = Indexer.instance();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        
+        String location = "http://localhost:42042/";
+        String key = System.getProperty("key");
+        if (key == null) return;
+        CendariIndexer cendari = new CendariIndexer(location, key);
+        List<Object> packages = cendari.getDataspaces();
+        for (Object o: packages) {
+            Map<String,Object> p = (Map<String,Object>)o;
+            System.out.println("Package is:");
+            try {
+                System.out.println(mapper.writeValueAsString(p));
+            } catch (JsonGenerationException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (JsonMappingException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+
+            ResourceList res = cendari.getResourceList((String)p.get("resources"));
+            for (Resource r : res)
+                try {
+                    System.out.println(mapper.writeValueAsString(r));
+                    byte[] content = cendari.getData((String)r.get("dataUrl"));
+                    if (content == null) {
+                        System.out.println("Cannot get content of dataUrl"+(String)r.get("dataUrl"));
+                        continue;
+                    }
+                    DocumentInfo info = indexer.parseDocument((String)r.get("name"), null, content, -1);
+                    System.out.println(mapper.writeValueAsString(info));
+                    info.setGroups_allowed((String)p.get("name"));
+                    indexer.indexDocument(info);
+                    
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+        }
+    }
+
+}
