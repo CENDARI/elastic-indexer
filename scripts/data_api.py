74ddad4c3908a44492860ab30092d4a7380ff8b1
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-01-24 14:08:13 +0100

    test data api

--- scripts/data_api.py
+++ scripts/data_api.py
@@ -3,13 +3,14 @@ import urllib2
 import urllib
 import json
 import pprint
+import sys
 
 # Use the json module to dump a dictionary to a string for posting.
 #data_string = urllib.quote(json.dumps({'id': 'data-explorer'}))
 
 # Make the HTTP request.
-response = urllib2.urlopen('http://localhost:42042/'+argv[0])
-request.add_header('Authorization', argv[1])
+response = urllib2.urlopen('http://localhost:42042/'+sys.argv[0])
+request.add_header('Authorization', sys.argv[1])
 assert response.code == 200
 
 # Use the json module to load CKAN's response into a dictionary.
